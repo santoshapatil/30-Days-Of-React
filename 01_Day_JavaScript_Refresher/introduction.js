e7e14abea12a7f200f7439b89483d30342269139
santoshapatil <santoshapatil31@gmail.com>
2021-07-27 12:21:17 +0530

    day1

---
+++ 01_Day_JavaScript_Refresher/introduction.js
@@ -0,0 +1,27 @@
+console.log('Welcome to 30DaysOfJavaScript',"YOLO SANTOSH")
+console.log("yolo SANTOSH")
+const a=Array()
+console.log(a)
+const numbers = [0, 3.14, 9.81, 37, 98.6, 100] // array of numbers
+const fruits = ['banana', 'orange', 'mango', 'lemon'] // array of strings, fruits
+const vegetables = ['Tomato', 'Potato', 'Cabbage', 'Onion', 'Carrot'] // array of strings, vegetables
+const animalProducts = ['milk', 'meat', 'butter', 'yoghurt'] // array of strings, products
+const webTechs = ['HTML', 'CSS', 'JS', 'React', 'Redux', 'Node', 'MongDB'] // array of web technologies
+const countries = ['Finland', 'Denmark', 'Sweden', 'Norway', 'Iceland'] // array of strings, countries
+console.log(numbers,animalProducts,webTechs)
+
+
+console.log('Numbers:', numbers)
+console.log('Number of numbers:', numbers.length)
+
+const arr = [
+    'Asabeneh',
+    250,
+    true,
+    { country: 'Finland', city: 'Helsinki' },
+    { skills: ['HTML', 'CSS', 'JS', 'React', 'Python'] },
+  ] // arr containing different data types
+console.log(arr)
+
+const eightEmptyValues = Array(8) // it creates eight empty values
+console.log(eightEmptyValues) // [empty x 8]
